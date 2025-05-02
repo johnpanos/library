abca302d75dfb569949e56da30b1cb07881c79ee
John Panos <me@johnpanos.com>
2025-05-01 17:08:16 -0700

    jp: implement Book Service and test

--- src/domain/services/BookService.ts
+++ src/domain/services/BookService.ts
@@ -1,5 +1,7 @@
-import type { Book } from "../models/Book.ts";
-import type { Result } from "../../types.ts";
+import { z, ZodError } from "zod";
+import type { Book, BookWithAvailableCopies } from "../models/Book.ts";
+import type { BookRepository } from "@/data/BookRepository.ts";
+import type { Result } from "@/types.ts";
 
 type CreateBookArgs = {
   title?: string;
@@ -7,8 +9,59 @@ type CreateBookArgs = {
   isbn?: string;
   copies?: number;
 };
+
+const CreateBookSchema = z.object({
+  title: z.string(),
+  author: z.string(),
+  isbn: z.string().and(z.string().length(10).or(z.string().length(13))),
+  copies: z.number().min(1),
+});
+
 export class BookService {
-  public createBook(bookArgs: CreateBookArgs): Result<Book> {
-    return { ok: false, error: new Error("Not Implemented") };
+  private bookRepository: BookRepository;
+
+  constructor(bookRepository: BookRepository) {
+    this.bookRepository = bookRepository;
+  }
+
+  public createBook(bookArgs: CreateBookArgs): Result<Book, ZodError | Error> {
+    const { data, error, success } = CreateBookSchema.safeParse(bookArgs);
+
+    if (!success) {
+      return { ok: false, error };
+    }
+
+    const existingBook = this.bookRepository.findByIsbn(data.isbn);
+
+    if (existingBook) {
+      return {
+        ok: false,
+        error: new Error(`Book with the ISBN ${bookArgs.isbn} already exists`),
+      };
+    }
+
+    const book = this.bookRepository.insert(data);
+
+    return {
+      ok: true,
+      value: book,
+    };
+  }
+
+  public getBook(isbn: string): Book | undefined {
+    return this.bookRepository.findByIsbn(isbn);
+  }
+
+  public getBookWithAvailableCopies(
+    isbn: string,
+  ): BookWithAvailableCopies | undefined {
+    const book = this.getBook(isbn);
+
+    if (!book) {
+      return;
+    }
+
+    // TODO: Actually calculate available copies
+    return { ...book, availableCopies: book.copies };
   }
 }
---
+++ tests/domain/services/BookService.test.ts
@@ -0,0 +1,72 @@
+import { expect, test } from "bun:test";
+import { BookService } from "@/domain/services/BookService.ts";
+import { createTestApplicationContext } from "../../TestEnvironment.ts";
+import { ZodError } from "zod";
+
+test("Create Book invalid", async () => {
+  const { bookRepository: repository } = createTestApplicationContext();
+  const service = new BookService(repository);
+
+  const result = service.createBook({});
+
+  expect(result.ok).toEqual(false);
+
+  if (!result.ok) {
+    const error = result.error as ZodError;
+
+    expect(error.formErrors.fieldErrors).toContainAllKeys([
+      "title",
+      "author",
+      "isbn",
+      "copies",
+    ]);
+  }
+});
+
+test("Create Book valid (13 digit ISBN format)", async () => {
+  const { bookRepository: repository } = createTestApplicationContext();
+  const service = new BookService(repository);
+
+  const expected = {
+    title: "Discrete Math",
+    author: "John Panos",
+    isbn: "123456789ABCD",
+    copies: 5,
+  };
+  const result = service.createBook(expected);
+
+  expect(result.ok).toEqual(true);
+});
+
+test("Create Book valid (10 digit ISBN format)", async () => {
+  const { bookRepository: repository } = createTestApplicationContext();
+  const service = new BookService(repository);
+
+  const expected = {
+    title: "Discrete Math",
+    author: "John Panos",
+    isbn: "123456789A",
+    copies: 5,
+  };
+  const result = service.createBook(expected);
+
+  expect(result.ok).toEqual(true);
+});
+
+test("Create Book invalid ISBN length", async () => {
+  const { bookRepository: repository } = createTestApplicationContext();
+  const service = new BookService(repository);
+
+  const expected = {
+    title: "Discrete Math",
+    author: "John Panos",
+    isbn: "123",
+    copies: 5,
+  };
+  const result = service.createBook(expected);
+
+  if (!result.ok) {
+    const error = result.error as ZodError;
+    expect(error.formErrors.fieldErrors).toContainAllKeys(["isbn"]);
+  }
+});
