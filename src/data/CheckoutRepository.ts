bb561125c9e01d2a846f8b7fa71435f1cdc10755
John Panos <me@johnpanos.com>
2025-05-01 17:50:08 -0700

    jp: stub out checkout repository

--- src/data/CheckoutRepository.ts
+++ src/data/CheckoutRepository.ts
@@ -1,3 +1,14 @@
 import type { ResettableRepository } from "@/data/ResettableRepository.ts";
+import type { Checkout } from "@/domain/models/Checkout.ts";
+import type { Customer } from "@/domain/models/Customer.ts";
+import type { Book } from "@/domain/models/Book.ts";
 
-export interface CheckoutRepository extends ResettableRepository {}
+export interface CheckoutRepository extends ResettableRepository {
+  insert(checkout: Omit<Checkout, "id">): Checkout;
+  update(checkout: Checkout): Checkout;
+
+  findById(id: Checkout["id"]): Checkout | undefined;
+  findByCustomer(customer: Customer): Checkout[];
+  findByBook(book: Book): Checkout[];
+  findByBookAndCustomer(customer: Customer, book: Book): Checkout[];
+}
--- src/data/in_memory/InMemoryCheckoutRepository.ts
+++ src/data/in_memory/InMemoryCheckoutRepository.ts
@@ -1,6 +1,33 @@
 import type { CheckoutRepository } from "@/data/CheckoutRepository.ts";
+import type { Book } from "@/domain/models/Book";
+import type { Checkout } from "@/domain/models/Checkout";
+import type { Customer } from "@/domain/models/Customer";
 
 export class InMemoryCheckoutRepository implements CheckoutRepository {
+  insert(checkout: Checkout): Checkout {
+    throw new Error("Method not implemented.");
+  }
+
+  return(checkout: Checkout, returnDate: Date): Checkout {
+    throw new Error("Method not implemented.");
+  }
+
+  findById(id: Checkout["id"]): Checkout | undefined {
+    throw new Error("Method not implemented.");
+  }
+
+  findByCustomer(customer: Customer): Checkout[] {
+    throw new Error("Method not implemented.");
+  }
+
+  findByBook(book: Book): Checkout[] {
+    throw new Error("Method not implemented.");
+  }
+
+  findCheckoutByBookAndCustomer(customer: Customer, book: Book): Checkout[] {
+    throw new Error("Method not implemented.");
+  }
+
   reset(): void {
     throw new Error("Method not implemented.");
   }
