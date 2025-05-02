d6f25ad6cd3be2be818aeaeac22ed34ecb61b426
John Panos <me@johnpanos.com>
2025-05-01 17:28:52 -0700

    jp: implement and test CustomerRepository

--- src/data/in_memory/InMemoryCustomerRepository.ts
+++ src/data/in_memory/InMemoryCustomerRepository.ts
@@ -2,15 +2,21 @@ import type { CustomerRepository } from "@/data/CustomerRepository.ts";
 import type { Customer } from "@/domain/models/Customer";
 
 export class InMemoryCustomerRepository implements CustomerRepository {
+  private customerMap: Record<string, Customer> = {};
+
   insert(customer: Customer): Customer {
-    throw new Error("Method not implemented.");
+    const customerCopy = { ...customer };
+
+    this.customerMap[customerCopy.id] = customerCopy;
+
+    return customerCopy;
   }
 
-  findById(customerId: Customer["id"]): Customer {
-    throw new Error("Method not implemented.");
+  findById(customerId: Customer["id"]): Customer | undefined {
+    return this.customerMap[customerId];
   }
 
   reset(): void {
-    throw new Error("Method not implemented.");
+    this.customerMap = {};
   }
 }
--- tests/data/CustomerRepository.test.ts
+++ tests/data/CustomerRepository.test.ts
@@ -15,3 +15,37 @@ test("insert", async () => {
 
   expect(actual).toEqual(expected);
 });
+
+test("findById", async () => {
+  const { customerRepository: repository } = createTestApplicationContext();
+  const expected: Customer = {
+    id: "CUST001",
+    name: "John Panos",
+    email: "me@johnpanos.com",
+  };
+
+  repository.insert(expected);
+
+  const actual = repository.findById(expected.id);
+
+  expect(actual).toEqual(expected);
+});
+
+test("reset", async () => {
+  const { customerRepository: repository } = createTestApplicationContext();
+  const expected: Customer = {
+    id: "CUST001",
+    name: "John Panos",
+    email: "me@johnpanos.com",
+  };
+
+  repository.insert(expected);
+
+  const actual = repository.findById(expected.id);
+
+  expect(actual).toEqual(expected);
+
+  repository.reset();
+
+  expect(repository.findById(expected.id)).toBeUndefined();
+});
